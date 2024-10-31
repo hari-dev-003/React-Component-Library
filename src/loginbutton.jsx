f0f642c811ac1616e13e22eaff59ec68e151c6e1
Hari Dev <hariharidev5@gmail.com>
2024-10-31 18:26:10 +0530

    Login Button

---
+++ src/loginbutton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loginbutton = () => {
+ const handleclick=()=>{
+    window.alert("This is an alert to ensure that button is working")
+  }
+  return (
+    <>
+    {/* Here I have added margin for the button (ml-80)*/}
+    <button className='w-20 h-10 rounded-2xl bg-blue-700 hover:scale-110
+    ml-80 scroll-smooth text-white' onClick={()=>handleclick()}>Login</button>
+    </>
+  )
+}
+
+export default Loginbutton
